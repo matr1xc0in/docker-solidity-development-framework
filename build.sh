5cab6a141edc86eecded35ec42e30f1490784607
matr1xc0in <>
2020-04-01 00:28:09 -0700

    Add package-lock.json file, re-tag image with branch name

--- build.sh
+++ build.sh
@@ -2,10 +2,12 @@
 
 curr_dir=$(cd $(dirname $0); pwd)
 
+BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD )
+
 pushd $curr_dir
 
 docker build \
-  -t blcksync/docker-solidity-development-framework:latest \
+  -t blcksync/docker-solidity-development-framework:$BRANCH_NAME \
   -f Dockerfile \
   .
 
